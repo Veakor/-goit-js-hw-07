821f0fd1a3105227bb2bc2da8e810d6951d79d6b
Veakor <135655247+Veakor@users.noreply.github.com>
2024-01-08 15:07:19 +0100

    add

--- js/task-6.js
+++ js/task-6.js
@@ -1,7 +1,15 @@
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+function isValidAmount(amount) {
+  return Number.isInteger(amount) && amount >= 1 && amount <= 100;
+}
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
+ return `#${Math.floor(Math.random() * 16777215)
+   .toString(16)
+     .padStart(6, 0)}`;
   }
 
   const input = document.querySelector('input');
@@ -40,4 +48,6 @@ function getRandomHexColor() {
         warning.remove();
       }, 2000);
     }
-  }
+  }//
+
+
